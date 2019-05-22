4876197f97f293a19e6179d7071327c3ecb12495
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-22 11:14:34 -0400

    append shapes, add attribute chaining

--- 1_svg-and-d3-basics/index.js
+++ 1_svg-and-d3-basics/index.js
@@ -2,9 +2,32 @@ import * as d3 from 'd3'
 
 const canvas = d3.select('.canvas')
 
-const svg = canvas.append('svg')
+const svg = canvas
+  .append('svg')
+  .attr('width', 600)
+  .attr('height', 600)
 
 // append shapes to SVG container
-svg.append('rect')
-svg.append('circle')
-svg.append('line')
+svg
+  .append('rect')
+  .attr('x', 20)
+  .attr('y', 20)
+  .attr('width', 200)
+  .attr('height', 100)
+  .style('fill', 'blue')
+
+svg
+  .append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .style('fill', 'pink')
+
+svg
+  .append('line')
+  .attr('x1', 370)
+  .attr('y1', 20)
+  .attr('x2', 400)
+  .attr('y2', 120)
+  .style('stroke', 'grey')
+  .style('stroke-width', 3)
