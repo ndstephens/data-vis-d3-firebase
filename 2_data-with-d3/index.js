4d9b486adfb671c5bc25e7762cbd17b0deb412ac
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-22 12:24:59 -0400

    join data to multiple elements

--- 2_data-with-d3/index.js
+++ 2_data-with-d3/index.js
@@ -1,15 +1,19 @@
 import * as d3 from 'd3'
 
 //? DATA
-const data = [{ width: 200, height: 100, fill: 'purple' }]
+const data = [
+  { width: 200, height: 100, fill: 'purple' },
+  { width: 100, height: 60, fill: 'pink' },
+  { width: 50, height: 30, fill: 'red' },
+]
 
 const svg = d3.select('svg')
 
 const rect = svg
-  .select('rect')
+  .selectAll('rect')
   .data(data)
   .attr('width', (d, i, n) => d.width)
   .attr('height', d => d.height)
   .style('fill', d => d.fill)
 
-console.log({ rect })
+console.log(rect)
