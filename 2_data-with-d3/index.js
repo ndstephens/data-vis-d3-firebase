571cb46de2255d52814fbbf25490f02091cef4e1
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-22 11:47:06 -0400

    connect shape attr's to data, use func return val

--- 2_data-with-d3/index.js
+++ 2_data-with-d3/index.js
@@ -1,1 +1,15 @@
 import * as d3 from 'd3'
+
+//? DATA
+const data = [{ width: 200, height: 100, fill: 'purple' }]
+
+const svg = d3.select('svg')
+
+const rect = svg
+  .select('rect')
+  .data(data)
+  .attr('width', d => d.width)
+  .attr('height', d => d.height)
+  .style('fill', d => d.fill)
+
+console.log({ rect })
