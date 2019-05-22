06b5830259757c2f15b90f195d8161b1ea62a57b
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-22 17:02:43 -0400

    groups and margins -- create a bar chart

--- 3_scales-and-bar-charts/index.js
+++ 3_scales-and-bar-charts/index.js
@@ -1,28 +1,49 @@
 import * as d3 from 'd3'
 
-const svg = d3.select('svg')
-svg.style('border', '1px solid black')
+//? CREATE THE MAIN SVG CONTAINER ELEMENT
+const svgWidth = 600
+const svgHeight = 600
 
+const svg = d3
+  .select('.canvas')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+  .style('border', '1px solid black')
+
+//? DEFINE MARGINS AND DIMENSIONS
+const margin = { top: 20, right: 20, bottom: 100, left: 100 }
+const graphWidth = svgWidth - margin.left - margin.right
+const graphHeight = svgHeight - margin.top - margin.bottom
+
+//? CREATE THE 'GRAPH' GROUP
+const graph = svg
+  .append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+//? FETCH DATA FILE, RETURN PROMISE, PROCESS DATA
 d3.json('./menu-data.json').then(data => {
-  // const extent = d3.extent(data, d => d.orders)
+  // Get the max value of all 'order' properties in the data array
   const ordersMax = d3.max(data, d => d.orders)
 
-  // Create a LINEAR scale function for the y-direction (height)
+  //* Create a LINEAR scale function for the y-direction (height)
   const yScale = d3
     .scaleLinear()
-    .domain([0, ordersMax])
-    .range([0, 500])
+    .domain([0, ordersMax]) // use the 'ordersMax' value
+    .range([0, graphHeight])
 
-  // Create a BAND scale function for the x-direction (num of bars)
+  //* Create a BAND scale function for the x-direction (num of bars)
   const xScale = d3
     .scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, 600])
+    .domain(data.map(item => item.name)) // provides quantity and a prop name
+    .range([0, graphWidth])
     .paddingInner(0.2)
-    .paddingOuter(0.2)
+  // .paddingOuter(0.2)
 
-  // join data to rects
-  const rects = svg.selectAll('rect').data(data)
+  // join the 'data' to 'rects'
+  const rects = graph.selectAll('rect').data(data)
 
   // add properties to any 'rect' elements already in DOM
   rects
@@ -31,7 +52,7 @@ d3.json('./menu-data.json').then(data => {
     .attr('x', d => xScale(d.name))
     .style('fill', 'orange')
 
-  // append the enter selection to the DOM
+  // append the 'enter' selection to the DOM with 'rect' elements
   rects
     .enter()
     .append('rect')
