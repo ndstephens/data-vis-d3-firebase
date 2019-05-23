9783f3909987cf06164b8a490b286710f340c02d
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-23 17:13:10 -0400

    add data to Firestore

---
+++ 6_Donut-Chart-project/index.js
@@ -0,0 +1,38 @@
+import db from './firebase'
+
+//? Cache UI elements
+const form = document.querySelector('form')
+const name = document.querySelector('#name')
+const cost = document.querySelector('#cost')
+const error = document.querySelector('#error')
+
+//? Event Listeners
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  // Display error if either input is empty
+  if (!name.value.trim() || !cost.value.trim()) {
+    error.textContent = 'Please enter values before submitting'
+  }
+
+  // Create an 'item' object
+  const item = {
+    name: name.value.trim(),
+    cost: parseInt(cost.value.trim(), 10),
+  }
+
+  // Save to db, reset form
+  db.collection('expenses')
+    .add(item)
+    .then(res => {
+      form.reset()
+      name.focus()
+    })
+})
+
+name.addEventListener('input', () => {
+  error.textContent = ''
+})
+cost.addEventListener('input', () => {
+  error.textContent = ''
+})
