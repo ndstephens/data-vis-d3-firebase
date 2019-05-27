e55ac7244df73a5b896d1686626580ee7e31148f
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-27 12:56:07 -0400

    add data to Firestore

--- 7_Line-Graph-project/index.js
+++ 7_Line-Graph-project/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable prefer-destructuring */
+import db from './firebase'
+
 //? DOM ELEMENTS
 const btns = Array.from(document.querySelectorAll('button'))
 const form = document.querySelector('form')
@@ -9,6 +11,7 @@ const error = document.querySelector('.error')
 // Default activity - updated by button clicks
 let activity = 'running'
 
+//? BUTTON EVENT LISTENERS
 btns.forEach(btn => {
   btn.addEventListener('click', e => {
     // get activity associated with button
@@ -23,5 +26,36 @@ btns.forEach(btn => {
 
     // update text in form
     formActivity.textContent = activity
+
+    // focus input
+    input.focus()
   })
+})
+
+//? FORM SUBMIT
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const distance = parseFloat(input.value.trim(), 10)
+  console.log(distance)
+
+  if (!distance) {
+    error.textContent = 'Please enter a valid distance'
+  } else {
+    db.collection('activities')
+      .add({
+        distance,
+        activity,
+        date: new Date().toString(),
+      })
+      .then(res => {
+        form.reset()
+        input.focus()
+      })
+  }
+})
+
+//? Reset error when typing
+input.addEventListener('input', e => {
+  error.textContent = ''
 })
