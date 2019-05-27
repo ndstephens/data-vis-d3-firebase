c7e06954e97bb7076b44e3868e5df0173cb393fb
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-27 13:54:11 -0400

    create scales and axes

--- 7_Line-Graph-project/graph.js
+++ 7_Line-Graph-project/graph.js
@@ -2,7 +2,8 @@
 import * as d3 from 'd3'
 import db from './firebase'
 
-//* CREATE UI SVG, MARGIN, and GRAPH SETTINGS
+//* ======== CHART USER INTERFACE ==========
+//? CREATE UI SVG, MARGIN, and GRAPH SETTINGS
 const svgDims = { width: 560, height: 400 }
 const margin = { top: 40, right: 20, bottom: 50, left: 100 }
 const graphDims = {
@@ -10,7 +11,7 @@ const graphDims = {
   height: svgDims.height - margin.top - margin.bottom,
 }
 
-//* CREATE OUR SVG and GRAPH / CHART GROUP
+//? CREATE OUR SVG and GRAPH / CHART GROUP
 const svg = d3
   .select('.canvas')
   .append('svg')
@@ -23,6 +24,22 @@ const graph = svg
   .attr('height', graphDims.height)
   .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
+//
+
+//* ======== SCALES AND AXES ==========
+const xScale = d3.scaleTime().range([0, graphDims.width])
+const yScale = d3.scaleLinear().range([graphDims.height, 0])
+
+const xAxisGroup = graph
+  .append('g')
+  .attr('class', 'x-axis')
+  .attr('transform', `translate(0, ${graphDims.height})`)
+
+const yAxisGroup = graph.append('g').attr('class', 'y-axis')
+
+//
+
+//* ======== APPLICATION LOGIC ==========
 //? UPDATE FUNCTION
 const update = data => {
   console.log(data)
