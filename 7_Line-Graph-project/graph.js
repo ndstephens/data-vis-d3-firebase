915d88fe3dfcfa46f3886a73a039e6cbd43d61ac
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-27 13:24:07 -0400

    create svg container and graph group

--- 7_Line-Graph-project/graph.js
+++ 7_Line-Graph-project/graph.js
@@ -2,12 +2,33 @@
 import * as d3 from 'd3'
 import db from './firebase'
 
+//* CREATE UI SVG, MARGIN, and GRAPH SETTINGS
+const svgDims = { width: 560, height: 400 }
+const margin = { top: 40, right: 20, bottom: 50, left: 100 }
+const graphDims = {
+  width: svgDims.width - margin.left - margin.right,
+  height: svgDims.height - margin.top - margin.bottom,
+}
+
+//* CREATE OUR SVG and GRAPH / CHART GROUP
+const svg = d3
+  .select('.canvas')
+  .append('svg')
+  .attr('width', svgDims.width)
+  .attr('height', svgDims.height)
+
+const graph = svg
+  .append('g')
+  .attr('width', graphDims.width)
+  .attr('height', graphDims.height)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
 //? UPDATE FUNCTION
 const update = data => {
   console.log(data)
 }
 
-// Declare a data array
+//? Declare a data array
 let data = []
 
 //? WATCH DATABASE FOR CHANGES IN REAL-TIME
