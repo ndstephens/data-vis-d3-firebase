943e0858f4f9900bc797397ddf650ac858404f58
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-28 18:25:35 -0400

    create svg element and graph group

--- 9_Tree-Diagram-project/graph.js
+++ 9_Tree-Diagram-project/graph.js
@@ -1,5 +1,22 @@
 /* eslint-disable no-case-declarations */
+import * as d3 from 'd3'
 import db from './firebase'
+
+//? UI CONSTANTS
+const graphDims = { width: 1100, height: 500 }
+const margins = { width: 100, height: 100 }
+
+//? CREATE SVG CONTAINER ELEMENT
+const svg = d3
+  .select('.canvas')
+  .append('svg')
+  .attr('width', graphDims.width + margins.width)
+  .attr('height', graphDims.height + margins.height)
+
+//? APPEND GRAPH GROUP TO SVG
+const graph = svg
+  .append('g')
+  .attr('transform', `translate(${margins.width / 2}, ${margins.height / 2})`)
 
 //? DATA OBJECT
 let data = []
